f206b2a5bf063309c1b51442311322e85938d9c4
Ryo Sugimoto <sugryo1109@gmail.com>
2014-08-29 18:08:17 +0900

    Add sass, scss, yaml-mode

---
+++ Cask
@@ -0,0 +1,66 @@
+(source gnu)
+(source melpa)
+(source marmalade)
+
+(depends-on "bind-key")
+(depends-on "cask")
+(depends-on "dash")
+(depends-on "drag-stuff")
+(depends-on "exec-path-from-shell")
+(depends-on "expand-region")
+(depends-on "f")
+(depends-on "flycheck")
+(depends-on "flycheck-cask")
+(depends-on "htmlize")
+(depends-on "idle-highlight-mode")
+(depends-on "magit")
+(depends-on "multiple-cursors")
+(depends-on "nyan-mode")
+(depends-on "pallet")
+(depends-on "popwin")
+(depends-on "prodigy")
+(depends-on "projectile")
+(depends-on "s")
+(depends-on "smartparens")
+(depends-on "smex")
+(depends-on "use-package")
+(depends-on "web-mode")
+(depends-on "yasnippet")
+
+;; Modes
+(depends-on "coffee-mode")
+(depends-on "js2-mode")
+(depends-on "json-mode")
+(depends-on "slim-mode")
+(depends-on "haml-mode")
+(depends-on "ruby-mode")
+(depends-on "markdown-mode")
+(depends-on "clojure-mode")
+(depends-on "yaml-mode")
+(depends-on "sass-mode")
+(depends-on "scss-mode")
+
+;; Complete setting
+(depends-on "auto-complete")
+
+;; git setting
+(depends-on "magit")
+(depends-on "popup")
+(depends-on "git-commit-mode")
+(depends-on "git-rebase-mode")
+(depends-on "gitconfig-mode")
+(depends-on "gitignore-mode")
+(depends-on "gh")
+(depends-on "gist")
+
+;; Ruby setting
+(depends-on "ruby-block")
+(depends-on "ruby-electric")
+(depends-on "ruby-end")
+
+;; Flymake-ruby
+(depends-on "flymake-ruby")
+(depends-on "rubocop")
+
+;; Twitter
+(depends-on "twittering-mode")
---
+++ init.el
@@ -0,0 +1,89 @@
+;; utf-8 にする
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+
+;; Mozc Setting
+(require 'mozc)
+(setq default-input-method "japanese-mozc")
+(prefer-coding-system 'utf-8)
+
+;; Rubyの時、標準でutf-8になるから
+;; マジックコメントいらない
+(setq ruby-insert-encoding-magic-comment nil)
+
+;; C-hで字を消せるようにする
+(keyboard-translate ?\C-h ?\C-?)
+
+;; C-tでウィンドウ切り替え
+(define-key global-map (kbd "C-t") 'other-window)
+
+;; カラム番号表示
+(column-number-mode t)
+
+;; 時計(24時間表示)
+(setq display-time-day-and-date t)
+(setq display-time-24hr-format t)
+(display-time-mode t)
+
+;; バッテリ残量表示(今は、いらないかも)
+(display-battery-mode t)
+
+;; ファイルサイズを表示
+(size-indication-mode t)
+
+;; Cask Setting
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; Ruby Setting
+(add-to-list 'auto-mode-alist '("\\.rb$latex" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(setq ruby-indent-level 2)
+(setq ruby-indent-tabs-mode t)
+(setq tab-width 2)
+
+;; Ruby-electric Setting
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list t)
+
+;; Ruby-block Setting
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highloght-toggle 'nothing)
+
+;; Auto-Complete Setting
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+  "~/ac-dict")
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+(ac-config-default)
+
+;; Flymake-Ruby Setting
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+;; Flycheck-Ruby Setting
+(setq flycheck-check-syntax-automatically '(mode-enabled save))
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+
+;; Markdown-mode Setting
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; CoffeeScript-mode Setting
+(defun coffee-custom () "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2)))
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+;; Rubocop Setting
+(add-hook 'ruby-mode-hook 'rubycop-mode)
+
+;; Twittering-mode Setting
+(setq twittering-use-master-password t)
+
+;; Ruby-end
+(require 'ruby-end)
